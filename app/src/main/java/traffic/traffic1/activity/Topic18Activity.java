80950ab14f730e74af446d0af4aa25bbd561698f
hkb <939939939@qq.com>
2017-05-12 12:05:24 +0800

    Merge remote-tracking branch 'remotes/origin/master'
    # Conflicts:
    #	app/src/main/AndroidManifest.xml

--- app/src/main/java/traffic/traffic1/activity/Topic18Activity.java
+++ app/src/main/java/traffic/traffic1/activity/Topic18Activity.java
@@ -74,7 +74,6 @@ public class Topic18Activity extends AppCompatActivity {
                     }
                 }
             }
-            
         }, new Response.ErrorListener() {
             @Override
             public void onErrorResponse(VolleyError error) {
